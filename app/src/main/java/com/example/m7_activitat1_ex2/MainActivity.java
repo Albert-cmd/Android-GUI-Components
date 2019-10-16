7862d37f6ca58da12bd14ff8ba85a471a06e9544
Albert Gonzalez <albert1999dual@gmail.com>
2019-10-16 11:06:42 +0200

    Ej terminado, con los botones y el spinner programado.

--- app/src/main/java/com/example/m7_activitat1_ex2/MainActivity.java
+++ app/src/main/java/com/example/m7_activitat1_ex2/MainActivity.java
@@ -3,20 +3,66 @@ package com.example.m7_activitat1_ex2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.textfield.TextInputEditText;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Spinner spinner = (Spinner) findViewById(R.id.selector);
+        spinner.setOnItemSelectedListener(this);
 
-        Spinner selector = (Spinner) findViewById(R.id.spinner);
 
     }
 
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
 
+            if (parent.getItemAtPosition(position)==0){
+
+                TextInputEditText quadreText = (TextInputEditText) findViewById(R.id.quadreTexthint);
+                quadreText.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD);
+
+            }
+
+        if (parent.getItemAtPosition(position)==1){
+
+            TextInputEditText quadreText = (TextInputEditText) findViewById(R.id.quadreTexthint);
+            quadreText.setInputType(InputType.TYPE_CLASS_NUMBER);
+
+        }
+
+        if (parent.getItemAtPosition(position)==2){
+
+            TextInputEditText quadreText = (TextInputEditText) findViewById(R.id.quadreTexthint);
+            quadreText.setInputType(InputType.TYPE_CLASS_PHONE);
+
+        }
+
+        if (parent.getItemAtPosition(position)==3){
+
+            TextInputEditText quadreText = (TextInputEditText) findViewById(R.id.quadreTexthint);
+            quadreText.setInputType(InputType.TYPE_CLASS_DATETIME);
+
+        }
+
+
+
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
